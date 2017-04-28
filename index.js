c0e0b49b40130a8aad7f996d294f5ce5527e676e
Salone Gupta <salonegupta@gmail.com>
2017-04-28 22:58:56 +0530

    Add socket support to make app detect if a user has connected to it

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var express = require('express')
 var http = require('http')
+var sio = require('socket.io');
 
 app = express()
 server = http.Server(app)
+io = sio(server)
+
+var connectedUsers = 0
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>')
+    res.sendFile(__dirname + '/index.html');
 })
 
+io.on('connection', function(socket){
+    connectedUsers++
+    console.log('A user has joined. Total Users: ' + connectedUsers);
+
+    socket.on('disconnect', function(){
+        connectedUsers--
+        console.log('A user has left. Total Users: ' + connectedUsers);
+    });
+});
+
 server.listen(3000, function(){
-    console.log('chat app listening on :3000')
+    console.log('Chat app listening on :3000')
 })
