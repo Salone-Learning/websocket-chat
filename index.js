82e3d53bfdda1ae915a7ce95f869f5ae77e63084
Salone Gupta <salonegupta@gmail.com>
2017-04-29 00:40:28 +0530

    Maintain login and logout details of users

--- index.js
+++ index.js
@@ -7,30 +7,56 @@ server = http.Server(app)
 io = sio(server)
 
 var connectedUsers = 0
+var socketMap = {}
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
 
 io.on('connection', (socket) => {
-    connectedUsers++
-    console.log('A user has joined. Total Users: ' + connectedUsers);
-    socket.broadcast.emit('system', 'A user has joined');
+    socket.on('login', (msg) => {
+        var user = msg
+
+        if (user) {
+            connectedUsers++
+            socketMap[socket.id] = user
+
+            console.log(user + ' has joined. Total Users: ' + connectedUsers);
+            socket.broadcast.emit('system', user + ' has joined');
+        }
+    })
 
     socket.on('message', (msg) => {
-        console.log('Message: ' + msg);
+        var values = msg.split(":")
+        var user = values[0]
+        var message = values[1]
+
+        console.log('User: ' + user + ' Message: ' + msg);
         socket.broadcast.emit('message', msg);
     });
 
     socket.on('typing', (msg) => {
-        console.log('User is typing');
+        var user = msg
+
+        console.log(user + ' is typing');
         socket.broadcast.emit('typing', msg);
     });
 
+    socket.on('logout', (msg) => {
+        var user = msg
+
+        console.log(user + ' has left. Total Users: ' + connectedUsers);
+        socket.broadcast.emit('system', user + ' has left');
+    })
+
     socket.on('disconnect', () => {
-        connectedUsers--
-        console.log('A user has left. Total Users: ' + connectedUsers);
-        socket.broadcast.emit('system', 'A user has left');
+        var user = socketMap[socket.id]
+
+        if (user) {
+            connectedUsers--
+            console.log(user + ' has left. Total Users: ' + connectedUsers);
+            socket.broadcast.emit('system', user + ' has left');
+        }
     });
 });
 
