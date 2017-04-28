fa26d602e97d5905a95875e6f9f14b4bd7f0fe90
Salone Gupta <salonegupta@gmail.com>
2017-04-29 00:11:30 +0530

    Add user, system and typing state message broadcasting features

--- index.js
+++ index.js
@@ -12,16 +12,28 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 })
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
     connectedUsers++
     console.log('A user has joined. Total Users: ' + connectedUsers);
+    socket.broadcast.emit('system', 'A user has joined');
 
-    socket.on('disconnect', function(){
+    socket.on('message', (msg) => {
+        console.log('Message: ' + msg);
+        socket.broadcast.emit('message', msg);
+    });
+
+    socket.on('typing', (msg) => {
+        console.log('User is typing');
+        socket.broadcast.emit('typing', msg);
+    });
+
+    socket.on('disconnect', () => {
         connectedUsers--
         console.log('A user has left. Total Users: ' + connectedUsers);
+        socket.broadcast.emit('system', 'A user has left');
     });
 });
 
-server.listen(3000, function(){
+server.listen(3000, () => {
     console.log('Chat app listening on :3000')
 })
